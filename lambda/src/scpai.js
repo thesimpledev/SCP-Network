0b2427959de16ca9e78f7fb70c11175dd009ad33
Steven Stanton <steven.stanton@gmail.com>
2023-08-20 17:50:02 -0400

    Added login and conversation tracking

--- lambda/src/scpai.js
+++ lambda/src/scpai.js
@@ -3,26 +3,42 @@ const {getSecret} = require('./util');
 
 const aiCall = async (body) => {
     let aikey = await getSecret('openaikey');
-
-    let prompt = body.command;
     const openai = new OpenAI({
-        apiKey:aikey, 
-      });
-      
-
-
-    const completion  =  await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {role: "system", content: "You are an AI for the SCP Foundation. You are tasked with answering questions about SCPs and other Foundation-related topics. Use Markdown to formnat all of your responses. please"},
-            {role: "user", content: prompt}
-        ],
+        apiKey: aikey,
     });
-    console.log("completion")
-    console.log(completion.choices[0].message.content)
-    responseMessage = completion.choices[0].message.content;
-    response = JSON.stringify({message: responseMessage});
+
+    // Initialize the messages array with the system message
+    let messages = [
+        {
+            role: "system",
+            content: `You are an AI for the SCP Foundation. 
+                      You are tasked with assisting Foundation personnel with their duties,
+                      questions about SCPs and other Foundation-related topics. 
+                      The user has O5 clearance.
+                      Use Markdown to format all of your responses, please.`
+        }
+    ];
+
+    // Add user and assistant messages from the conversation history to the messages array
+    for (let msg of body.conversation) {
+        messages.push({
+            role: msg.type === 'user' ? 'user' : 'assistant',  // Convert 'server' to 'assistant'
+            content: msg.message
+        });
+    }
+
+    const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo-16k",
+        messages: messages
+    });
+
+    console.log("completion");
+    console.log(completion.choices[0].message.content);
+    const responseMessage = completion.choices[0].message.content;
+    const response = JSON.stringify({ message: responseMessage });
     return response;
 };
+
+
 
 module.exports = { aiCall };
--- site/scripts.js
+++ site/scripts.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    localStorage.clear();
     const terminalInput = document.querySelector('.terminal-input');
     const outputDiv = document.querySelector('.output');
     let accumulatedInput = "";
+    const FAUX_USERNAME = 'admin';
+    const FAUX_PASSWORD = 'container';
+    const postItUsername = document.querySelector('.post-it p:nth-child(1)');
+    const postItPassword = document.querySelector('.post-it p:nth-child(2)');
+
+    postItUsername.innerHTML = `<strong>Username:</strong> ${FAUX_USERNAME}`;
+    postItPassword.innerHTML = `<strong>Password:</strong> ${FAUX_PASSWORD}`;
+
+    let loginState = 'username';  // Possible values: 'username', 'password', 'authenticated'
+
 
     function startLoadingAnimation() {
         let count = 0;
@@ -13,14 +24,15 @@ document.addEventListener('DOMContentLoaded', () => {
         return loadingInterval;
     }
 
-    async function sendCommandToServer(command) {
+    async function sendConversationToServer() {
+        const conversation = JSON.parse(localStorage.getItem('conversation') || '[]');
         try {
             const response = await fetch('https://3enuvpqp3l.execute-api.us-east-2.amazonaws.com/alpha', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ command: command })
+                body: JSON.stringify({ conversation: conversation })
             });
 
             if (!response.ok) {
@@ -51,13 +63,46 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
 
-    terminalInput.addEventListener('keydown', async function(e) {
-        if (e.key === 'Enter' && e.shiftKey) {
-            accumulatedInput += terminalInput.value + '\n';
-            terminalInput.value = '';
-        } else if (e.key === 'Enter') {
-            e.preventDefault();
+    document.querySelector('.prompt').textContent = 'Username:';  // Set initial prompt
+
+terminalInput.addEventListener('keydown', async function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+
+        // Handle the username input
+        if (loginState === 'username') {
+            if (terminalInput.value === FAUX_USERNAME) {
+                accumulatedInput = terminalInput.value;  // Store the username for later use
+                terminalInput.value = '';
+                document.querySelector('.prompt').textContent = 'Password:';
+                loginState = 'password';
+            } else {
+                terminalInput.value = '';
+                outputDiv.innerHTML += `<p>Error: Incorrect username. Try again.</p>`;
+            }
+
+        // Handle the password input
+        } else if (loginState === 'password') {
+            if (terminalInput.value === FAUX_PASSWORD) {
+                terminalInput.value = '';
+                outputDiv.innerHTML += `<p>Welcome ${accumulatedInput}!</p>`;
+                document.querySelector('.prompt').textContent = 'scp>';
+                loginState = 'authenticated';
+            } else {
+                terminalInput.value = '';
+                accumulatedInput = '';  // Clear the previously stored username
+                outputDiv.innerHTML += `<p>Error: Incorrect password. Try again.</p>`;
+                document.querySelector('.prompt').textContent = 'Username:';
+                loginState = 'username';
+            }
+
+        // Handle command input (once authenticated)
+        } else if (loginState === 'authenticated') {
             accumulatedInput += terminalInput.value;
+             // Store user input in localStorage
+            let conversation = JSON.parse(localStorage.getItem('conversation') || '[]');
+            conversation.push({ type: 'user', message: accumulatedInput });
+            localStorage.setItem('conversation', JSON.stringify(conversation));
             
             // Check if the command exists in our custom commands
             if (commandFunctions[accumulatedInput.trim().toLowerCase()]) {
@@ -68,20 +113,34 @@ document.addEventListener('DOMContentLoaded', () => {
             outputDiv.innerHTML += `<p>scp&gt; ${marked.parse(accumulatedInput).replace(/<p>|<\/p>/g, '')}</p>`;
             
             const loadingInterval = startLoadingAnimation();
-            const serverResponse = await sendCommandToServer(accumulatedInput);
+            const serverResponse = await sendConversationToServer();
     
             clearInterval(loadingInterval);
             terminalInput.value = '';
     
             if (serverResponse.error) {
                 outputDiv.innerHTML += `<p>Error: ${serverResponse.error}</p>`;
+                
+                // Store error response in localStorage
+                conversation = JSON.parse(localStorage.getItem('conversation') || '[]');
+                conversation.push({ type: 'assistant', message: `Error: ${serverResponse.error}` });
+                localStorage.setItem('conversation', JSON.stringify(conversation));
+    
             } else {
                 outputDiv.innerHTML += marked.parse(serverResponse.message);
+    
+                // Store server response in localStorage
+                conversation = JSON.parse(localStorage.getItem('conversation') || '[]');
+                conversation.push({ type: 'assistant', message: serverResponse.message });
+                localStorage.setItem('conversation', JSON.stringify(conversation));
             }
     
             accumulatedInput = "";
         }
-    });
+    }
+});
+
+    
 
     async function ls() {
         const mockFiles = [
