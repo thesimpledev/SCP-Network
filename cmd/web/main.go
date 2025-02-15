4655b09f60dffd235550d10f06168c276e35a145
sstanton <steven@thesimpledev.com>
2025-02-15 11:41:18 -0500

    Configured the OnMount for the application to setup the initial state on load

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,15 +4,40 @@ import (
 	"github.com/maxence-charriere/go-app/v10/pkg/app"
 )
 
-type hello struct {
+type applicationStep int
+
+const (
+	loginStep applicationStep = iota
+	passwordStep
+	terminalStep
+)
+
+type application struct {
 	app.Compo
+
+	step        applicationStep
+	userInput   string
+	outputLines []string
+
+	correctUserName string
+	correctPassword string
+
+	loading        bool
+	loadingMessage string
 }
 
-func (h *hello) Render() app.UI {
+func (r *application) OnMount(ctx app.Context) {
+	r.correctUserName = "aclef"
+	r.correctPassword = "container"
+	r.step = loginStep
+	r.outputLines = []string{}
+}
+
+func (h *application) Render() app.UI {
 	return app.H1().Text("Hello, World! How are you doing today?")
 }
 
 func main() {
-	app.Route("/", func() app.Composer { return &hello{} })
+	app.Route("/", func() app.Composer { return &application{} })
 	app.RunWhenOnBrowser()
 }
